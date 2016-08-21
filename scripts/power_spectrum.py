dec8cb6a6337928937f13a8105c7c2aab0228c32
MeteoBoy4 <lichiqin4@gmail.com>
2016-08-21 18:16:07 +0800

    Ajust average plot_name to make naming more automatic

--- scripts/power_spectrum.py
+++ scripts/power_spectrum.py
@@ -17,9 +17,9 @@ def main(args=sys.argv[1:]):
 
     target_file = xr.open_dataset(args.input)
     variable = target_file[target_file.keys()[-1]]
-    variable_slice = variable.sel(longitude=np.arange(60, 120), latitude=np.arange(-10, 60), time=slice('2013-06-01', '2013-08-31'))
+    variable_slice = variable.sel(longitude=np.arange(120, 180), latitude=np.arange(30, 60), time=slice('2005-01-08', '2005-01-11'))
 
-    latitude_range = range(20, 41, 1)
+    latitude_range = range(30, 60, 1)
     for i, lat in enumerate(latitude_range):
         fft2 = fft.FFT(variable_slice.sel(latitude=lat), latitude=lat, folder=args.output)
         if i == 0:
@@ -28,9 +28,9 @@ def main(args=sys.argv[1:]):
             power += fft2.shiftized
         fft2.draw()
     power /= len(latitude_range)
-    fft.draw(target=power, folder=args.output, latitude=lat, plot_name='20-40mean', xcood=fft2.xcood, ycood=fft2.ycood)
+    fft.draw(target=power, folder=args.output, latitude=lat, plot_name='{0}-{1}mean'.format(latitude_range[0], latitude_range[-1]), xcood=fft2.xcood, ycood=fft2.ycood)
 
-    longitude_range = range(80, 100, 1)
+    longitude_range = range(120, 160, 1)
     for i, lon in enumerate(longitude_range):
         fft2 = fft.FFT(variable_slice.sel(longitude=lon), longitude=lon, folder=args.output)
         if i == 0:
@@ -39,7 +39,7 @@ def main(args=sys.argv[1:]):
             power += fft2.shiftized
         fft2.draw()
     power /= len(longitude_range)
-    fft.draw(target=power, folder=args.output, longitude=lon, plot_name='80-100mean', xcood=fft2.xcood, ycood=fft2.ycood)
+    fft.draw(target=power, folder=args.output, longitude=lon, plot_name='{0}-{1}mean'.format(longitude_range[0], longitude_range[-1]), xcood=fft2.xcood, ycood=fft2.ycood)
 
 if __name__ == '__main__':
     main()
