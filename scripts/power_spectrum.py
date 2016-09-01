85c61d2773b8aaeaf077f4e78147a11f01bc9334
MeteoBoy4 <lichiqin4@gmail.com>
2016-09-01 09:56:58 +0800

    add the plotting level functionality

--- scripts/power_spectrum.py
+++ scripts/power_spectrum.py
@@ -16,41 +16,46 @@ def main(args=sys.argv[1:]):
                         help="the latitude range for analysis, must be two numbers indicting the maximum and minimum")
     parser.add_argument("--longitude", nargs=2, type=float, default=[120., 180.],
                         help="the longitude range for analysis, must be two numbers indicting the minimum and maximum")
+    parser.add_argument("--level", nargs=3, type=float,
+                        help="the contour levels you wish to draw, indicating the minimum, maximum and spacing respectively")
 
     args = parser.parse_args()
     north_latitude = args.latitude[0]
     south_latitude = args.latitude[1]
     west_longitude = args.longitude[0]
     east_longitude = args.longitude[1]
-    folder_name = args.output + '_lat{0}-{1}_lon{2}-{3}'.format(south_latitude, north_latitude, west_longitude, east_longitude)
+    levels = np.linspace(args.level[0], args.level[1], args.level[2])
+    folder_name = args.output + '_lat{0}-{1}_lon{2}-{3}'.format(south_latitude, north_latitude, west_longitude,
+                                                                east_longitude)
     print(folder_name)
     target_file = xr.open_dataset(args.input)
     variable = target_file[target_file.keys()[-1]]
-    variable_slice = variable.sel(longitude=slice(west_longitude, east_longitude), latitude=slice(north_latitude, south_latitude),
+    variable_slice = variable.sel(longitude=slice(west_longitude, east_longitude),
+                                  latitude=slice(north_latitude, south_latitude),
                                   time=slice('2005-01-08', '2005-01-11'))
 
     latitude_range = np.arange(south_latitude, north_latitude, 1)
     for i, lat in enumerate(latitude_range):
-        fft2 = fft.FFT2(variable_slice.sel(latitude=lat), latitude=lat, folder=folder_name)
+        fft2 = fft.FFT2(variable_slice.sel(latitude=lat), latitude=lat, folder=folder_name, levels=levels)
         if i == 0:
             power = fft2.shiftized
         else:
             power += fft2.shiftized
         fft2.draw()
     power /= len(latitude_range)
-    fft.draw(target=power, folder=folder_name, latitude=lat,
+    fft.draw(target=power, folder=folder_name, latitude=lat, levels=levels,
              plot_name='{0}-{1}mean'.format(latitude_range[0], latitude_range[-1]), xcood=fft2.xcood, ycood=fft2.ycood)
 
     longitude_range = np.arange(west_longitude, east_longitude, 1)
     for i, lon in enumerate(longitude_range):
-        fft2 = fft.FFT2(variable_slice.sel(longitude=lon), longitude=lon, folder=folder_name)
+        fft2 = fft.FFT2(variable_slice.sel(longitude=lon), longitude=lon, folder=folder_name, levels=levels)
         if i == 0:
             power = fft2.shiftized
         else:
             power += fft2.shiftized
         fft2.draw()
     power /= len(longitude_range)
-    fft.draw(target=power, folder=folder_name, longitude=lon,
+    fft.draw(target=power, folder=folder_name, longitude=lon, levels=levels,
              plot_name='{0}-{1}mean'.format(longitude_range[0], longitude_range[-1]), xcood=fft2.xcood,
              ycood=fft2.ycood)
 
